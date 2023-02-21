1b2a0ddb26450b8057e16d7af6dbb5c490afe83b
davidokloh <oklohokopi888@gmail.com>
2023-02-21 14:13:45 +0100

    An educational website

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("scientific").style.display="none"
+document.getElementById("artist").style.display="none"
+document.getElementById("commercialist").style.display="none"
+
+function showOrHide() {
+    var subjects=document.getElementById("subjects").value 
+    if(subjects=="science"){
+        document.getElementById("scientific").style.display="block"
+        document.getElementById("artist").style.display="none"
+        document.getElementById("commercialist").style.display="none"
+    }else if(subjects=="art"){
+        document.getElementById("artist").style.display="block"
+        document.getElementById("scientific").style.display="none"
+        document.getElementById("commercialist").style.display="none"
+    }else if(subjects=="commercial"){
+        document.getElementById("commercialist").style.display="block" 
+        document.getElementById("artist").style.display="none" 
+        document.getElementById("scientist").style.display="none" 
+    }
+}
